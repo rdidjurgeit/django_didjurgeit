076702dbd111fe8b4d569d0dc6a3353131d24a7f
rdidjurgeit <didjurgeit.raphael@gmail.com>
2024-10-16 22:34:59 +0200

    Configure whitenoise for static files
    See #12

--- my_project/settings.py
+++ my_project/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,8 +140,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-if 'STATIC_ROOT' in os.environ:
-    STATIC_ROOT = os.environ.get('STATIC_ROOT')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
