427001551ba2e89be97ed66ec7dc7ecd53cf1af1
rdidjurgeit <raphael0rd@gmail.com>
2024-10-17 04:11:49 +0200

    Forgot Debug False

--- my_project/settings.py
+++ my_project/settings.py
@@ -28,7 +28,7 @@ if 'SECRET_KEY' in os.environ:
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", '.herokuapp.com']
 
